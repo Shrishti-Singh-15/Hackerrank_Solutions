66b3cfa78c57d1e5a66f7b92bfa2a8daadb440c3
Shrishti_Singh_15 <shrishti.singh@watermelon.us>
2022-08-14 15:36:04 +0530

    commit

---
+++ Java/Java_Stdin_Stdout_II.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.lang.String;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double a = sc.nextDouble();
+        sc.nextLine();
+        String str = sc.nextLine();
+        System.out.println("String: "+str);
+        System.out.println("Double: "+a);
+        System.out.println("Int: "+n);
+    }
+}
